dec7261aa4c931805cb8df87b3d92396b08caf5e
Geizielder <geizielder.gervazio@gmail.com>
2022-11-25 16:28:00 -0300

    mod 2 assync/await

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ function trataErro(erro) {
     throw new Error(chalk.red(erro.code, 'não há arquivo no diretório.'));
 }
 
+// async/await
+
+async function pegaArquivo(caminhoDoArquivo) {
+    try {
+        const encoding = 'utf-8';
+        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+        console.log(chalk.green(texto));
+        
+    } catch (erro) {
+        trataErro(erro);
+    }
+
+}
+
 // function pegaArquivo(caminhoDoArquivo){
 //     const encoding = 'utf-8'; 
 //     fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
@@ -16,8 +30,11 @@ function trataErro(erro) {
 //     })
 // }
 
-function pegaArquivo(caminhoDoArquivo) {
-    const encoding = 'utf-8';
-    fs.promises.readFile(caminhoDoArquivo, encoding).then((texto) => console.log(chalk.green(texto))).catch(trataErro);
-}
+//Usando then
+
+// function pegaArquivo(caminhoDoArquivo) {
+//     const encoding = 'utf-8';
+//     fs.promises.readFile(caminhoDoArquivo, encoding).then((texto) => console.log(chalk.green(texto))).catch(trataErro);
+// }
 pegaArquivo('./arquivos/texto.md');
+pegaArquivo('./arquivos/');
